139964744c87f2aa105c673c08b7456f441fafa9
Ozeas <ozeaspe@gmail.com>
2024-09-05 08:11:09 -0300

    start1
    ...

--- lwc learning/force-app/main/default/lwc/memoryGameLwc/memoryGameLwc.js
+++ lwc learning/force-app/main/default/lwc/memoryGameLwc/memoryGameLwc.js
@@ -1,3 +1,21 @@
 import { LightningElement } from 'lwc';
+import {loadStyle} from 'lightning/platformResourceLoader'
+import fontawesome from '@salesforce/resourceUrl/fontawesome'
 
-export default class MemoryGameLwc extends LightningElement {}
+
+export default class MemoryGameLwc extends LightningElement {
+
+    isLibLoaded = false
+    renderedCallback(){
+        if(this.isLibLoaded){
+            return
+        }else{
+            loadStyle(this,fontawesome+'/fontawesome/css/font-awesome.min.css').then(()=>{
+                console.log("Load Successfully")
+            }).catch(error=>{
+                console.error(error)
+            })
+            this.isLibLoaded
+        }
+    }
+}
